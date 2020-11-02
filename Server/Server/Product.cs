a59f097441c3f6969e79b9fb237c25e29aec8bb5
Jonas <jsvare19@student.aau.dk>
2020-11-02 14:09:48 +0100

    Reverted interface

--- Server/Server/Product.cs
+++ Server/Server/Product.cs
@@ -1,25 +1,17 @@
 ﻿namespace Server
 {
-    public class Product : IProductDescription
+    public class Product
     {
-        public int amount;
 
-        public int id { get; set; }
-        public string name { get; set; }
-        public int volume { get; set; }
-        public int weight { get; set; }
-        public int colli { get; set; }
-        public string color { get; set; }
+        ProductDescription description { get; }
+        public int amount { get; set; }
 
-        public Product(int productID, string productName, int productVolume, int productWeight, int productColli, string productColor)
+        public Product(ProductDescription d)
         {
-            id = productID;
-            name = productName;
-            volume = productVolume;
-            weight = productWeight;
-            colli = productColli;
-            color = productColor;
+            description = d;
+            amount = 0;
         }
+        
 
         //public Product(int productID, string productName, int productVolume, int productWeight, int productColli, string productColor) : base(productID, productName, productVolume, productWeight, productColli, productColor) { }
         //ProductDescription fuck = new ProductDescription(123, "sdfs", 123, 123, 1, "red");
