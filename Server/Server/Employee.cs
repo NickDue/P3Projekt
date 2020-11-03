8334cdb6c9c1605947af823ecf60e6fbe7b39465
Jonas <jsvare19@student.aau.dk>
2020-11-03 14:17:24 +0100

    Added Employee DB functionalities

--- Server/Server/Employee.cs
+++ Server/Server/Employee.cs
@@ -1,18 +1,22 @@
-﻿namespace Server
+﻿using System.Collections.Generic;
+
+namespace Server
 {
     public class Employee
     {
-        public int _employeeID { get; set; }
-        public string _password { get; set; }
-        public string _employeeName { get; set; }
-        public Role _role { get; set; }
+        public static List<Employee> employeeList = new List<Employee>();
+
+        public int employeeID { get; set; }
+        public string password { get; set; }
+        public string employeeName { get; set; }
+        public Role role { get; set; }
 
         public Employee(int employeeID, string password, string employeeName, Role role)
         {
-            _employeeID = employeeID;
-            _password = password;
-            _employeeName = employeeName;
-            _role = role;
+            this.employeeID = employeeID;
+            this.password = password;
+            this.employeeName = employeeName;
+            this.role = role;
         }
 
         void CreateEmployee(int employeeID, string password, string name, string role)
@@ -22,8 +26,8 @@
 
         public enum Role
         {
-            floorWorker,
-            officeWorker
+            FloorWorker,
+            OfficeWorker
         }
     }
 }
--- Server/Server/LocalStorage/LoadDatabaseFiles.cs
+++ Server/Server/LocalStorage/LoadDatabaseFiles.cs
@@ -4,18 +4,13 @@ namespace Server.LocalStorage
 {
     public class LoadDatabaseFiles
     {
-        public static void load()
+        public static void Load()
         {
             // todo: enter the functions
-            loadProducts();
+            LoadProducts();
         }
 
-        private static void loadAccounts()
-        {
-            
-        }
-
-        private static void loadProducts()
+        private static void LoadProducts()
         {
             string toSplit = AccessDatabase.ReadData("Test");
             string[] splitted = toSplit.Split("\n");
@@ -30,6 +25,18 @@ namespace Server.LocalStorage
                 Product.allProducts.Add(productSplitted[0],p );
             }
         }
-        
+
+        private static void LoadEmployees()
+        {
+            string toSplit = AccessDatabase.ReadData("EmployeeTest");
+            string[] splitted = toSplit.Split("\n");
+            for (int i = 0; i < splitted.Length - 1; i++)
+            {
+                string[] employeeSplitted = splitted[i].Split("?");
+                Console.WriteLine(employeeSplitted[0]);
+                Employee em = new Employee(Int32.Parse(employeeSplitted[0]), employeeSplitted[1], employeeSplitted[2], (Employee.Role)Enum.Parse(typeof(Employee.Role),employeeSplitted[3]));
+
+            }
+        }
     }
 }
