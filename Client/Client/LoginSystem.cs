f86a80ef7025e9efa2ac1cb1f55d1f5c424e6633
Frederik Rasmussen <58684446+dunkedolmer@users.noreply.github.com>
2020-11-04 13:11:30 +0100

    Login System renamed
    Dolmer.

--- Client/Client/LoginSystem.cs
+++ Client/Client/LoginSystem.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Client
 {
-    public partial class UserControl1 : UserControl
+    public partial class LoginSystem : UserControl
     {
-        public UserControl1()
+        public LoginSystem()
         {
             InitializeComponent();
         }
