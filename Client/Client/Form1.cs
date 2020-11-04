df32de95c81e1cac614e197149a175f7232056b4
Kenneth Køpke <Kkapke19@student.aau.dk>
2020-11-04 14:00:48 +0100

    made functions smooth
    Kenneth

--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -20,35 +20,69 @@ namespace Client
         {
             searchWindow1.Hide();
         }
+        private void HighlightSelectedButton(Button button) 
+        {
+            RemoveHighlightButton();
+            button.BackColor = Color.Black;
+            button.ForeColor = Color.White;
+            
+        }
+        private void RemoveHighlightButton()
+        {
+            NavButton1.BackColor = Color.White;
+            NavButton1.ForeColor = Color.Black;
+            NavButton2.BackColor = Color.White;
+            NavButton2.ForeColor = Color.Black;
+            NavButton3.BackColor = Color.White;
+            NavButton3.ForeColor = Color.Black;
+            NavButton4.BackColor = Color.White;
+            NavButton4.ForeColor = Color.Black;
+            NavButton5.BackColor = Color.White;
+            NavButton5.ForeColor = Color.Black;
+            NavButton6.BackColor = Color.White;
+            NavButton6.ForeColor = Color.Black;
+            NavButton7.BackColor = Color.White;
+            NavButton7.ForeColor = Color.Black;
+        }
 
 
         private void NavButton1_Click(object sender, EventArgs e)
         {
             searchWindow1.Show();
+            HighlightSelectedButton(NavButton1);
         }
         private void NavButton2_Click(object sender, EventArgs e)
         {
             searchWindow1.Hide();
+            HighlightSelectedButton(NavButton2);
+
         }
         private void NavButton3_Click(object sender, EventArgs e)
         {
             searchWindow1.Hide();
+            HighlightSelectedButton(NavButton3);
         }
         private void NavButton4_Click(object sender, EventArgs e)
         {
             searchWindow1.Hide();
+            HighlightSelectedButton(NavButton4);
         }
         private void NavButton5_Click(object sender, EventArgs e)
         {
             searchWindow1.Hide();
+            HighlightSelectedButton(NavButton5);
+
         }
         private void NavButton6_Click(object sender, EventArgs e)
         {
             searchWindow1.Hide();
+            HighlightSelectedButton(NavButton6);
         }
         private void NavButton7_Click(object sender, EventArgs e)
         {
             searchWindow1.Hide();
+            HighlightSelectedButton(NavButton7);
+
         }
         private void Navigationbar_Paint(object sender, PaintEventArgs e)
         {
