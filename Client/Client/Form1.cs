7880cbf35e34c805d4498ef1b2cea7e197d007b1
Nick / AFK <56578594+NickDue@users.noreply.github.com>
2020-11-11 09:46:22 +0100

    Revert "Loginui added to solution"

--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -18,43 +18,99 @@ namespace Client
         }
         private void MyhomeForm_Load(object sender, EventArgs e)
         {
-            //searchWindow1.Hide();
+            HideNotSelectedPage();
+        }
+        private void HighlightSelectedButton(Button button) 
+        {
+            RemoveHighlightButton();
+            button.BackColor = Color.Black;
+            button.ForeColor = Color.White;
+            
+        }
+        private void RemoveHighlightButton()
+        {
+            NavButton1.BackColor = Color.White;
+            NavButton1.ForeColor = Color.Black;
+            NavButton2.BackColor = Color.White;
+            NavButton2.ForeColor = Color.Black;
+            NavButton3.BackColor = Color.White;
+            NavButton3.ForeColor = Color.Black;
+            NavButton4.BackColor = Color.White;
+            NavButton4.ForeColor = Color.Black;
+            NavButton5.BackColor = Color.White;
+            NavButton5.ForeColor = Color.Black;
+            NavButton6.BackColor = Color.White;
+            NavButton6.ForeColor = Color.Black;
+            NavButton7.BackColor = Color.White;
+            NavButton7.ForeColor = Color.Black;
         }
 
 
         private void NavButton1_Click(object sender, EventArgs e)
         {
+            HideNotSelectedPage();
             searchWindow1.Show();
+            HighlightSelectedButton(NavButton1);
         }
         private void NavButton2_Click(object sender, EventArgs e)
         {
-            searchWindow1.Hide();
+            HideNotSelectedPage();
+            addProductWindow1.Show();
+            HighlightSelectedButton(NavButton2);
+
         }
         private void NavButton3_Click(object sender, EventArgs e)
         {
-            searchWindow1.Hide();
+            HideNotSelectedPage();
+            allProductsPage1.Show();
+            HighlightSelectedButton(NavButton3);
         }
         private void NavButton4_Click(object sender, EventArgs e)
         {
-            searchWindow1.Hide();
+            HideNotSelectedPage();
+            createPicklistWindow1.Show();
+            HighlightSelectedButton(NavButton4);
         }
         private void NavButton5_Click(object sender, EventArgs e)
         {
-            searchWindow1.Hide();
+            HideNotSelectedPage();
+            employeesWindow1.Show();
+            HighlightSelectedButton(NavButton5);
+
         }
         private void NavButton6_Click(object sender, EventArgs e)
         {
-            searchWindow1.Hide();
+            HideNotSelectedPage();
+            logsPage1.Show();
+            HighlightSelectedButton(NavButton6);
         }
         private void NavButton7_Click(object sender, EventArgs e)
         {
-            searchWindow1.Hide();
+            HideNotSelectedPage();
+            HighlightSelectedButton(NavButton7);
+            logOutPage2.Show();
+            Timetitle.Show();
+            TimeLabel.Show();
+            TimeLabel.Text = DateTime.Now.ToLongTimeString();
+
         }
         private void Navigationbar_Paint(object sender, PaintEventArgs e)
         {
 
         }
-
+        
+        private void HideNotSelectedPage()
+        {
+            searchWindow1.Hide();
+            allProductsPage1.Hide();
+            logsPage1.Hide();
+            employeesWindow1.Hide();
+            addProductWindow1.Hide();
+            createPicklistWindow1.Hide();
+            logOutPage2.Hide();
+            TimeLabel.Hide();
+            Timetitle.Hide();
+        }
 
 
 
@@ -67,9 +123,5 @@ namespace Client
         {
 
         }
-
-
-
-        
     }
 }
