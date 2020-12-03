0b16b8ff87cb9a7a73fcb8ca45868dad85d61ffc
Nick / Oaky <56578594+NickDue@users.noreply.github.com>
2020-12-03 14:00:11 +0100

    added get all logs function

---
+++ Server/Server/Log.cs
@@ -0,0 +1,25 @@
+﻿using Org.BouncyCastle.Tsp;
+
+namespace Server
+{
+    public class Log
+    {
+        public string WorkerID { get; }
+        public string ProductId { get; }
+        public string Message { get; }
+        public string Date { get; }
+
+        public Log(string wId, string productId, string message, string date)
+        {
+            WorkerID = wId;
+            ProductId = productId;
+            Message = message;
+            Date = date;
+        }
+
+        public string LogToString()
+        {
+            return $"{WorkerID}!{ProductId}!{Message}!{Date}";
+        }
+    }
+}
