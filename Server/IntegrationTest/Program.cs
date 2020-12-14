5f392f8f9b2c54b2fd776bc5b8f97947f0d5ec4c
Nick / Oaky <56578594+NickDue@users.noreply.github.com>
2020-12-14 14:03:32 +0100

    added test product functions

---
+++ Server/IntegrationTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntegrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPServer server = new TCPServer();
+            server.Run();
+        }
+    }
+}
